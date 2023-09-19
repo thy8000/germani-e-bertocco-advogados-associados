5db6c88a104978735a94ac9557ed3e0803893cc6
Thunay <thunaymoreiradesoares@gmail.com>
2023-09-18 23:57:03 -0300

    ✨ Front Page - Hero

--- app/public/wp-content/themes/it9-theme-main/pages/front-page/_index.php
+++ app/public/wp-content/themes/it9-theme-main/pages/front-page/_index.php
@@ -4,13 +4,7 @@ if(!defined('ABSPATH')) exit;
 
 wp_utils_get_component('header/_index');
 
-?>
-
-<div class="w-full h-[500px] bg-black">
-    <div class="text-white text-[50px] font-black">Hello World</div>
-</div>
-
-<?php
+wp_utils_get_component('front-page/hero');
 
 wp_utils_get_component('footer/_index');
 
