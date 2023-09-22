1b5461ca80fabdc99514e63530f6916b0767ad48
Thunay <thunaymoreiradesoares@gmail.com>
2023-09-21 22:43:09 -0300

    ✨ Front Page - About Us

--- app/public/wp-content/themes/it9-theme-main/pages/front-page/_index.php
+++ app/public/wp-content/themes/it9-theme-main/pages/front-page/_index.php
@@ -6,6 +6,8 @@ wp_utils_get_component('header/_index');
 
 wp_utils_get_component('front-page/hero');
 
+wp_utils_get_component('front-page/about-us');
+
 wp_utils_get_component('footer/_index');
 
 ?>
