fa07884906f9e10f8249aa737825cc083595c458
Thunay <thunaymoreiradesoares@gmail.com>
2023-09-21 22:24:00 -0300

    💄 Front Page - Hero

--- app/public/wp-content/themes/it9-theme-main/includes/components/front-page/hero.php
+++ app/public/wp-content/themes/it9-theme-main/includes/components/front-page/hero.php
@@ -16,9 +16,13 @@ url(" . esc_url(wp_utils_get_image_link('hero-background.jpg')) . ");"
                     <?php esc_html_e('Germani e Bertocco') ?>
                 </h1>
 
-                <h2 class="max-w-3xl text-white text-6xl font-extrabold font-lato">
+                <h2 class="max-w-3xl pb-16 text-white text-6xl font-extrabold font-lato">
                     <?php esc_html_e('Escritório de Advocacia em Sâo Paulo') ?>
                 </h2>
+
+                <a class="p-5 bg-transparent border border-white rounded text-white it9-transition hover:bg-white hover:text-gray-600" href="<?php echo esc_url(get_page_by_path('contato')) ?>">
+                    <?php echo esc_html_e('Entrar em contato') ?>
+                </a>
             </div>
         </div>
     </div>
