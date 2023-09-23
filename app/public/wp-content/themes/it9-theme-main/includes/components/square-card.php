6b1467bc3f2ad6aab6e6de23bc5e6b7e9b22b0a9
Thunay <thunaymoreiradesoares@gmail.com>
2023-09-23 14:34:37 -0300

    💄 Square Card

--- app/public/wp-content/themes/it9-theme-main/includes/components/square-card.php
+++ app/public/wp-content/themes/it9-theme-main/includes/components/square-card.php
@@ -7,7 +7,7 @@ if (!defined('ABSPATH')) {
 ?>
 
 <div class="w-full flex flex-col justify-center items-center aspect-[286/281] bg-white">
-    <h2 class="text-6xl lg:text-4xl xl:text-6xl font-medium font-lato text-emperor">
+    <h2 class="text-6xl lg:text-4xl xl:text-6xl font-medium font-lato text-emperor italic">
         <?php esc_html_e($args['info']['big_title']) ?>
     </h2>
 
